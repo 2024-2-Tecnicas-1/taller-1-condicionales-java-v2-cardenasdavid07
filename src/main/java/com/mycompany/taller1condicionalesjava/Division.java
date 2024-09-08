ecf653ca79ec2f1d1be283c56e53dfdb821a3991
david.cardenas <david.cardenas0718@gmail.com>
2024-09-07 22:53:59 -0500

    se agrega codigo de años bisiestos y division faltantes

---
+++ src/main/java/com/mycompany/taller1condicionalesjava/Division.java
@@ -0,0 +1,39 @@
+package com.mycompany.taller1condicionalesjava;
+
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author danielsanchez
+ */
+public class Division {
+    public static String evaluar(int dividendo, int divisor) {
+       int cociente = dividendo / divisor;
+        int residuo = dividendo % divisor;
+
+        // Verifica si la división es exacta
+        String respuesta;
+        if (residuo == 0) {
+            respuesta = "La división es exacta.\n"
+                      + "Cociente: " + cociente + "\n"
+                      + "Residuo: " + residuo;
+        } else {
+            respuesta = "La división no es exacta.\n"
+                      + "Cociente: " + cociente + "\n"
+                      + "Residuo: " + residuo;
+        }
+
+        return respuesta;
+    }
+    public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in);
+        System.out.print("Dividendo:");
+        int dividendo = lector.nextInt();
+        System.out.print("Divisor:");
+        int divisor = lector.nextInt();
+
+        String respuesta = evaluar(dividendo, divisor);
+        System.out.println(respuesta);
+    }
+}
--- src/main/java/ejercicios/AnnosBisiestos.java
+++ src/main/java/ejercicios/AnnosBisiestos.java
@@ -10,9 +10,20 @@ import java.util.Scanner;
  */
 public class AnnosBisiestos {
     public static String evaluar(int anno){
-        // TODO: Coloca aquí el código del ejercicio 2: Años bisiestos
-        return "";
+      if (anno < 1582) {
+        if (anno % 4 == 0) {
+            return anno + " es bisiesto";
+        } else {
+            return anno + " no es bisiesto";
+        }
+    } else {
+        if ((anno % 4 == 0 && anno % 100 != 0) || anno % 400 == 0) {
+            return anno + " es bisiesto";
+        } else {
+            return anno + " no es bisiesto";
+        }
     }
+}
     
     public static void main(String[] args) {
         Scanner lector = new Scanner(System.in);
